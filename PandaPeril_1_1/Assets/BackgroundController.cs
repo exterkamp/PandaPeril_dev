ef3697e57fc2beca1ea451eaf31f044f72ed2193
Shane Exterkamp <gorefest5@gmail.com>
2015-03-04 20:23:23 -0500

    PandaPeril 1.1.3
    -Added parallax to some background clouds, they look really bad, but
    it's a start

---
+++ PandaPeril_1_1/Assets/BackgroundController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	public GameObject spawner;
+
+	public float baseSpeed = 0.025f;
+	public float maxSpeed = 0.1f;
+	//public float maxSpeedVariation = 0.2f;
+	
+	public float acceleration = 0.0025f;
+	public float deceleration = 0.025f;
+	
+	public float curSpeed;
+	public bool  stopFlag;
+
+	public GameObject[] prefab;
+	public float yMax;
+	public float yMin;
+	public float chanceOfSpawnPerUpdate;
+	private GameObject terrainControllerRef;
+
+	// Use this for initialization
+	void Start () {
+		curSpeed = baseSpeed;
+		stopFlag = false;
+		terrainControllerRef = GameObject.FindGameObjectWithTag ("BGController");
+		//StartCoroutine (spawnBG());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!Input.GetMouseButton (0)) {
+			stopFlag = false;
+			//panda.transform.Rotate (0, 0, curRotation * Time.deltaTime);
+			foreach (Transform child in transform) {
+				//child is your child transform
+				child.position = new Vector3 (child.position.x - curSpeed, child.position.y, 10);
+			}
+			if (curSpeed < maxSpeed){
+				curSpeed = curSpeed + acceleration;
+				//curRotation = curRotation + rotationAcc;
+			}
+			else if (curSpeed > maxSpeed){
+				curSpeed = maxSpeed;
+			}
+			if (Random.Range(0f,1f) <= chanceOfSpawnPerUpdate)
+			{
+				//spawnBG ();
+				Vector3 newPos = new Vector3 (spawner.transform.position.x, spawner.transform.position.y + Random.Range (yMin, yMax), spawner.transform.position.z);
+				
+				GameObject newBG = (GameObject)Instantiate (prefab [Random.Range (0, prefab.Length)], newPos, new Quaternion (0, 0, 0, 0));
+				
+				newBG.transform.parent = terrainControllerRef.transform;
+			}
+		} else {
+			if (!stopFlag)
+			{
+				//panda.transform.Rotate (0, 0, curRotation * Time.deltaTime);
+				foreach (Transform child in transform) {
+					//child is your child transform
+					child.position = new Vector3 (child.position.x - curSpeed, child.position.y, 10);
+				}
+				//panda.transform.position = transform.position + new Vector3 (curSpeed, 0.0f, 0.0f);
+			}
+			if (curSpeed > 0 && !stopFlag){
+				//curRotation -= rotationDec;
+				curSpeed -= deceleration;
+			}
+			else{
+				curSpeed = baseSpeed;
+				//curRotation = baseRotation;
+				stopFlag = true;
+			}
+			
+			
+			//curSpeed = baseSpeed;
+			//curRotation = baseRotation;
+		}
+		
+	}
+
+	/*IEnumerator spawnBG(){
+		//choose postion
+		while (true) {
+			Vector3 newPos = new Vector3 (this.transform.position.x, this.transform.position.y + Random.Range (yMin, yMax), this.transform.position.z);
+
+			GameObject newBG = (GameObject)Instantiate (prefab [Random.Range (0, prefab.Length)], newPos, new Quaternion (0, 0, 0, 0));
+
+			newBG.transform.parent = terrainControllerRef.transform;
+		}
+		//return Start ();
+	}*/
+
+}
--- PandaPeril_1_1/Assets/garbageCollect.cs
+++ PandaPeril_1_1/Assets/garbageCollect.cs
@@ -19,37 +19,41 @@ public class garbageCollect : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collidee)
 	{
-		Destroy(collidee.gameObject.transform.parent.gameObject);
-		//Rigidbody newTerrain;
-		Transform farthestRight = null;
-		foreach (Transform child in terrainControllerRef.transform) {
-			if (farthestRight != null){
-				if (farthestRight.position.x < child.position.x){
+		if (!collidee.gameObject.transform.parent.tag.Equals ("BGItem")) {
+
+
+			//Rigidbody newTerrain;
+
+			Transform farthestRight = null;
+			foreach (Transform child in terrainControllerRef.transform) {
+				if (farthestRight != null) {
+					if (farthestRight.position.x < child.position.x) {
+						farthestRight = child;
+					}
+				} else {
 					farthestRight = child;
 				}
-			}else{
-				farthestRight = child;
 			}
+
+			Vector3 newPos = new Vector3 (farthestRight.position.x + 50, 0, 0);
+
+
+			//Rigidbody newTerrain; 
+			GameObject newTerrain = (GameObject)Instantiate (prefab [Random.Range (0, prefab.Length)], newPos, new Quaternion (0, 0, 0, 0));
+			//Instantiate (obj [Random.Range (0, obj.GetLength (0))], transform.position, Quaternion.identity);
+
+
+
+			newTerrain.transform.parent = terrainControllerRef.transform;
+			//SpriteRenderer sr = (SpriteRenderer)newTerrain.GetComponent<SpriteRenderer> ();
+			//sr.color = new Color (Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f),1f);
+			//GameObject gO = clone.gameObject;
+			//SpriteRenderer[] sr = gO.GetComponents<SpriteRenderer>();
+			//if (sr != null) {
+			//	sr[0].color = new Color (Random.Range (0, 255), Random.Range (0, 255), Random.Range (0, 255));
+			//}
+			//Random rand = new Random ();
 		}
-
-		Vector3 newPos = new Vector3 (farthestRight.position.x + 50,0,0);
-
-
-		//Rigidbody newTerrain; 
-		GameObject newTerrain = (GameObject) Instantiate(prefab[Random.Range (0,prefab.Length)], newPos, new Quaternion(0,0,0,0));
-		//Instantiate (obj [Random.Range (0, obj.GetLength (0))], transform.position, Quaternion.identity);
-
-
-
-		newTerrain.transform.parent = terrainControllerRef.transform;
-		//SpriteRenderer sr = (SpriteRenderer)newTerrain.GetComponent<SpriteRenderer> ();
-		//sr.color = new Color (Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f),1f);
-		//GameObject gO = clone.gameObject;
-		//SpriteRenderer[] sr = gO.GetComponents<SpriteRenderer>();
-		//if (sr != null) {
-		//	sr[0].color = new Color (Random.Range (0, 255), Random.Range (0, 255), Random.Range (0, 255));
-		//}
-		//Random rand = new Random ();
-
+		Destroy (collidee.gameObject.transform.parent.gameObject);
 	}
 }
